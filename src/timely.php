2a4e7492a52df73c9b0d0e78b7b6f15a2413b475
teedodev <teemodev@gmail.com>
2014-12-15 10:04:50 +0900

    Add funtions has been added

--- src/timely.php
+++ src/timely.php
@@ -14,12 +14,17 @@ class Timely extends DateTime {
         );
 	private static $SUFFIX = "전";
 
-	public function __construct($timezone) {		
+	public function __construct($time = null, $timezone = null) {		
 		/*
 		if( is_null( $locale )) {
 		}
 		$this->LOCALE = $locale;
 		*/
+		if ( !is_null($timezone) ) {
+			parent::__construct($time);
+		} else {
+			parent::__construct($time);
+		}
 	}
 
 	public function setTime( $value ) {
@@ -55,7 +60,52 @@ class Timely extends DateTime {
 		$interval = $todatetime->diff(new DateTime( $fromdatetime ));
 		return static::_getRelativeTime( $interval );
 	}
-	
-}
 
+	public static function now($time = null) {
+        return new static( null, $tz );
+    }
+	
+	public function addYears( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addYears function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' year');
+	}
+
+	public function addMonths( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addMonths function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' month');
+	}
+
+	public function addDays( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addDays function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' day');
+	}
+
+	public function addHours( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addHours function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' hour');
+	}
+
+	public function addMinutes( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addMinutes function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' minute');
+	}
+
+	public function addSeconds($value)
+    {
+        if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addSeconds function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( intval($value) . ' second');
+    }
+}
 ?>
