7250781aeb8c855e5796f651830f96d33756c4a1
teedodev <teemodev@gmail.com>
2014-12-20 02:40:41 +0900

    Backup - Before get Calendar function

--- src/timely.php
+++ src/timely.php
@@ -5,21 +5,21 @@ class Timely extends DateTime {
 	private $TIMEZONE;
 	private $TIMEFORMAT = array ( "past" => "ago" );
 	private $FROMDATETIME;
-	private $THRESHOLDS = array(
-            "s" => 1,  // seconds to minute
-            "m" => 1,  // minutes to hour
-            "h" => 1,  // hours to day
-            "d" => 1,  // days to month
-            "M" => 1   // months to year
-        );
+	
 	private static $SUFFIX = "전";
 
-	public function __construct($time = null, $timezone = null) {		
-		/*
-		if( is_null( $locale )) {
-		}
-		$this->LOCALE = $locale;
-		*/
+	private static $DAYS = array(
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednsday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	);
+
+
+	public function __construct($time = null, $timezone = null) {			
 		if ( !is_null($timezone) ) {
 			parent::__construct($time);
 		} else {
@@ -42,7 +42,7 @@ class Timely extends DateTime {
 		if ( $intervalObj->y >= 1 ) { 
 			$relativeTimeResult = $intervalObj->y.static::pluralize( $intervalObj->y , "년" , true ); 
 		} else if ($intervalObj->m >= 1) {
-			$relativeTimeResult = $intervalObj->m.static::pluralize( $intervalObj->m , "월" , true ); 
+			$relativeTimeResult = $intervalObj->m.static::pluralize( $intervalObj->m , "개월" , true ); 
 		} else if ($intervalObj->d >= 1) {
 			$relativeTimeResult = $intervalObj->d.static::pluralize( $intervalObj->d , "일" , true ); 
 		} else if ($intervalObj->h >= 1) {
@@ -64,6 +64,36 @@ class Timely extends DateTime {
 	public static function now($time = null) {
         return new static( null, $tz );
     }
+
+	public static function Calendar( $year , $month ) {
+	}
+
+	public function getCalendar() {
+		return $this->month(1);
+	}
+
+	public function month ( $value ) {
+		$this->month = $value;
+		return $this;
+	}
+
+	public function day( $value ) {
+		$this->day = $value;
+		print_r($this);
+		return $this;
+	}
+
+	public function startDayOfMonth() {
+		$result = array(
+			"date" => 1,
+			"day" => $this->day(1)->format('w')
+		);
+		return $result; 
+	}
+
+	public function endDayOfMonth() {
+		return $this->daysInMonth;
+	}
 	
 	public function addYears( $value ) {
 		if ( !is_int( $value ) ) {
@@ -107,5 +137,115 @@ class Timely extends DateTime {
 		}
 		return $this->modify( intval($value) . ' second');
     }
+
+	public function subYears( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addYears function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 * intval($value) . ' year');
+	}
+
+	public function subMonths( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addMonths function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 *  intval($value) . ' month');
+	}
+
+	public function subDays( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addDays function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 * intval($value) . ' day');
+	}
+
+	public function subHours( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addHours function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 * intval($value) . ' hour');
+	}
+
+	public function subMinutes( $value ) {
+		if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addMinutes function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 * intval($value) . ' minute');
+	}
+
+	public function subSeconds($value) {
+        if ( !is_int( $value ) ) {
+			throw new InvalidArgumentException('addSeconds function only accepts integers. Input was: '.$value);
+		}
+		return $this->modify( -1 * intval($value) . ' second');
+    }
+
+	public function __get( $name ) {
+
+		switch ($name) {
+            case 'year':
+            case 'month':
+            case 'day':
+            case 'hour':
+            case 'minute':
+            case 'second':
+            case 'micro':
+            case 'dayOfWeek':
+            case 'dayOfYear':
+            case 'weekOfYear':
+            case 'daysInMonth':
+            case 'timestamp':
+                $formats = array(
+                    'year' => 'Y',
+                    'month' => 'n',
+                    'day' => 'j',
+                    'hour' => 'G',
+                    'minute' => 'i',
+                    'second' => 's',
+                    'micro' => 'u',
+                    'dayOfWeek' => 'w',
+                    'dayOfYear' => 'z',
+                    'weekOfYear' => 'W',
+                    'daysInMonth' => 't',
+                    'timestamp' => 'U',
+                );
+                return (int) $this->format($formats[$name]);
+			default:
+				throw new InvalidArgumentException(sprintf("Unknown getter '%s'", $name));
+				break;
+		}
+	}
+	public function __set( $name , $value) {
+		switch ($name) {
+            case 'year':
+                parent::setDate($value, $this->month, $this->day);
+                break;
+            case 'month':
+                parent::setDate($this->year, $value, $this->day);
+                break;
+            case 'day':
+                parent::setDate($this->year, $this->month, $value);
+                break;
+            case 'hour':
+                parent::setTime($value, $this->minute, $this->second);
+                break;
+            case 'minute':
+                parent::setTime($this->hour, $value, $this->second);
+                break;
+            case 'second':
+                parent::setTime($this->hour, $this->minute, $value);
+                break;
+            case 'timestamp':
+                parent::setTimestamp($value);
+                break;
+            case 'timezone':
+            case 'tz':
+                $this->setTimezone($value);
+                break;
+            default:
+                throw new InvalidArgumentException(sprintf("Unknown setter '%s'", $name));
+        }
+	}
+
 }
 ?>
