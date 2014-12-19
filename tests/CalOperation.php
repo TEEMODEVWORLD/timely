f1b3661ff951364a0b6cb323c074b1593452f61f
teedodev <teemodev@gmail.com>
2014-12-20 02:55:35 +0900

    Get Calendar function has been added

---
+++ tests/CalOperation.php
@@ -0,0 +1,13 @@
+<?
+	/*
+		* Test Timely 
+		*
+		* (c) Ray - teemodev@gmail.com
+		*
+		* For the full copyright and license information, please view the LICENSE
+	*/
+class calOperation extends PHPUnit_Framework_TestCase {
+		public function testAddDay() {
+		}
+}
+?>
